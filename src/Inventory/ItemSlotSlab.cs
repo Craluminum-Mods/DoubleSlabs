8c94fcaa0673e7c1b563814ddb438495eb612bda
Craluminum2413 <beware.14b@gmail.com>
2024-06-17 10:24:57 +0300

    fix crash

--- src/Inventory/ItemSlotSlab.cs
+++ src/Inventory/ItemSlotSlab.cs
@@ -10,11 +10,11 @@ public class ItemSlotSlab : ItemSlot
 
     public override bool CanTakeFrom(ItemSlot sourceSlot, EnumMergePriority priority = EnumMergePriority.AutoMerge)
     {
-        return sourceSlot.Itemstack.Collectible.IsSlab();
+        return sourceSlot?.Itemstack?.Collectible?.IsSlab() == true;
     }
 
     public override bool CanHold(ItemSlot sourceSlot)
     {
-        return sourceSlot.Itemstack.Collectible.IsSlab();
+        return sourceSlot?.Itemstack?.Collectible?.IsSlab() == true;
     }
 }
