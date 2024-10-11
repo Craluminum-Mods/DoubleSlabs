8cfb6dfe7d22c870b5f73ad2dee49ea599efba6b
Dana <beware.14b@gmail.com>
2024-10-11 11:39:15 +0300

    fix decal crash

--- src/Systems/HarmonyPatches.cs
+++ src/Systems/HarmonyPatches.cs
@@ -17,7 +17,7 @@ public class HarmonyPatches : ModSystem
         HarmonyInstance.CreateReversePatcher(original: Block_GetParticleCollisionBoxes_Patch.TargetMethod(), standin: typeof(Block_GetParticleCollisionBoxes_Patch).GetMethod(nameof(Block_GetParticleCollisionBoxes_Patch.Base))).Patch(HarmonyReversePatchType.Original);
         HarmonyInstance.CreateReversePatcher(original: Block_GetBlockMaterial_Patch.TargetMethod(), standin: typeof(Block_GetBlockMaterial_Patch).GetMethod(nameof(Block_GetBlockMaterial_Patch.Base))).Patch(HarmonyReversePatchType.Original);
         HarmonyInstance.CreateReversePatcher(original: Block_GetSounds_Patch.TargetMethod(), standin: typeof(Block_GetSounds_Patch).GetMethod(nameof(Block_GetSounds_Patch.Base))).Patch(HarmonyReversePatchType.Original);
-        HarmonyInstance.CreateReversePatcher(original: Block_GetDecal_Patch.TargetMethod(), standin: typeof(Block_GetDecal_Patch).GetMethod(nameof(Block_GetDecal_Patch.Base))).Patch(HarmonyReversePatchType.Original);
+        // HarmonyInstance.CreateReversePatcher(original: Block_GetDecal_Patch.TargetMethod(), standin: typeof(Block_GetDecal_Patch).GetMethod(nameof(Block_GetDecal_Patch.Base))).Patch(HarmonyReversePatchType.Original);
         HarmonyInstance.CreateReversePatcher(original: Block_DoEmitSideAo_Patch.TargetMethod(), standin: typeof(Block_DoEmitSideAo_Patch).GetMethod(nameof(Block_DoEmitSideAo_Patch.Base))).Patch(HarmonyReversePatchType.Original);
         HarmonyInstance.CreateReversePatcher(original: Block_DoEmitSideAoByFlag_Patch.TargetMethod(), standin: typeof(Block_DoEmitSideAoByFlag_Patch).GetMethod(nameof(Block_DoEmitSideAoByFlag_Patch.Base))).Patch(HarmonyReversePatchType.Original);
 
@@ -25,7 +25,7 @@ public class HarmonyPatches : ModSystem
         HarmonyInstance.Patch(original: Block_GetCollisionBoxes_Patch.TargetMethod(), prefix: typeof(Block_GetCollisionBoxes_Patch).GetMethod(nameof(Block_GetCollisionBoxes_Patch.Prefix)));
         HarmonyInstance.Patch(original: Block_GetParticleCollisionBoxes_Patch.TargetMethod(), prefix: typeof(Block_GetParticleCollisionBoxes_Patch).GetMethod(nameof(Block_GetParticleCollisionBoxes_Patch.Prefix)));
         HarmonyInstance.Patch(original: Block_OnBeingLookedAt_Patch.TargetMethod(), prefix: typeof(Block_OnBeingLookedAt_Patch).GetMethod(nameof(Block_OnBeingLookedAt_Patch.Prefix)));
-        HarmonyInstance.Patch(original: Block_GetDecal_Patch.TargetMethod(), prefix: typeof(Block_GetDecal_Patch).GetMethod(nameof(Block_GetDecal_Patch.Prefix)));
+        // HarmonyInstance.Patch(original: Block_GetDecal_Patch.TargetMethod(), prefix: typeof(Block_GetDecal_Patch).GetMethod(nameof(Block_GetDecal_Patch.Prefix)));
 
         HarmonyInstance.Patch(original: Block_DoParticalSelection_Patch.TargetMethod(), postfix: typeof(Block_DoParticalSelection_Patch).GetMethod(nameof(Block_DoParticalSelection_Patch.Postfix)));
         HarmonyInstance.Patch(original: Block_SideIsSolid_Patch.TargetMethod(), postfix: typeof(Block_SideIsSolid_Patch).GetMethod(nameof(Block_SideIsSolid_Patch.Postfix)));
@@ -41,21 +41,6 @@ public class HarmonyPatches : ModSystem
 
     public override void Dispose()
     {
-        HarmonyInstance.Unpatch(original: Block_GetSelectionBoxes_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetCollisionBoxes_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetParticleCollisionBoxes_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_OnBeingLookedAt_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetDecal_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-
-        HarmonyInstance.Unpatch(original: Block_DoParticalSelection_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_SideIsSolid_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_SideIsSolid_Patch2.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetLiquidBarrierHeightOnSide_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetSounds_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetBlockMaterial_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetLightAbsorption_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_GetLightAbsorption_Patch2.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_DoEmitSideAo_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
-        HarmonyInstance.Unpatch(original: Block_DoEmitSideAoByFlag_Patch.TargetMethod(), HarmonyPatchType.All, HarmonyInstance.Id);
+        HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
     }
 }
--- src/Systems/HarmonyPatches/Block_GetDecal_Patch.cs
+++ src/Systems/HarmonyPatches/Block_GetDecal_Patch.cs
@@ -7,32 +7,34 @@ using Vintagestory.API.MathTools;
 
 namespace DoubleSlabs;
 
-public static class Block_GetDecal_Patch
-{
-    public static MethodBase TargetMethod()
-    {
-        return AccessTools.Method(typeof(Block), nameof(Block.GetDecal));
-    }
+// causes too much issues, no idea how to fix
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public static void Base(this Block instance, IWorldAccessor world, BlockPos pos, ITexPositionSource decalTexSource, ref MeshData decalModelData, ref MeshData blockModelData)
-    {
-    }
+// public static class Block_GetDecal_Patch
+// {
+//     public static MethodBase TargetMethod()
+//     {
+//         return AccessTools.Method(typeof(Block), nameof(Block.GetDecal));
+//     }
 
-    public static bool Prefix(Block __instance, IWorldAccessor world, BlockPos pos, ITexPositionSource decalTexSource, ref MeshData decalModelData, ref MeshData blockModelData)
-    {
-        if (__instance.IsSlab()
-            && world.Api is ICoreClientAPI capi
-            && capi?.World?.Player?.CurrentBlockSelection?.SelectionBoxIndex == 1
-            && world.BlockAccessor.TryGetBEBehavior(pos, out BEBehaviorDoubleSlab bebehavior)
-            && !bebehavior.IsEmpty)
-        {
+//     [MethodImpl(MethodImplOptions.NoInlining)]
+//     public static void Base(this Block instance, IWorldAccessor world, BlockPos pos, ITexPositionSource decalTexSource, ref MeshData decalModelData, ref MeshData blockModelData)
+//     {
+//     }
 
-            decalModelData = BlockBehaviorDoubleSlab.GetOrCreateMesh(capi, __instance, bebehavior.ContainedBlock, decalTexSource).Clone();
-            blockModelData = BlockBehaviorDoubleSlab.GetOrCreateMesh(capi, __instance, bebehavior.ContainedBlock).Clone();
-            Base(bebehavior.ContainedBlock, world, pos, decalTexSource, ref decalModelData, ref blockModelData);
-            return false;
-        }
-        return true;
-    }
-}
+//     public static bool Prefix(Block __instance, IWorldAccessor world, BlockPos pos, ITexPositionSource decalTexSource, ref MeshData decalModelData, ref MeshData blockModelData)
+//     {
+//         if (__instance.IsSlab()
+//             && world.Api is ICoreClientAPI capi
+//             && capi?.World?.Player?.CurrentBlockSelection?.SelectionBoxIndex == 1
+//             && world.BlockAccessor.TryGetBEBehavior(pos, out BEBehaviorDoubleSlab bebehavior)
+//             && !bebehavior.IsEmpty)
+//         {
+
+//             decalModelData = BlockBehaviorDoubleSlab.GetOrCreateMesh(capi, __instance, bebehavior.ContainedBlock, decalTexSource).Clone();
+//             blockModelData = BlockBehaviorDoubleSlab.GetOrCreateMesh(capi, __instance, bebehavior.ContainedBlock).Clone();
+//             Base(bebehavior.ContainedBlock, world, pos, decalTexSource, ref decalModelData, ref blockModelData);
+//             return false;
+//         }
+//         return true;
+//     }
+// }
